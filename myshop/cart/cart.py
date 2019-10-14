67545af02e1bb3b8489ff4a0bcd52e88fbe9a43c
Krzysztof Formanowicz <formanowicz.k@gmail.com>
2019-10-14 18:50:16 +0400

    added shopping cart

---
+++ myshop/cart/cart.py
@@ -0,0 +1,77 @@
+from decimal import Decimal
+from django.conf import settings
+from myshop.shop.models import Product
+
+
+class Cart(object):
+    def __init__(self, request):
+        """
+        Initialize shopping cart
+        :param request:
+        """
+        self.session = request.session
+        cart = self.session.get(settings.CART_SESSION_ID)
+        if not cart:
+            cart = self.session[settings.CART_SESSION_ID] = {}
+        self.cart = cart
+
+    def __iter__(self):
+        """
+        Iterate over shopping cart elements and get products from db
+        :return:
+        """
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        for product in products:
+            self.cart[str(product.id)]['product'] = product
+
+        for item in self.cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+
+    def __len__(self):
+        """
+        Calculate the amount of items in shopping cart
+        :return:
+        """
+        return sum(item['quantity'] for item in self.cart.values())
+
+    def add(self, product, quantity=1, update_quantity=False):
+        """
+        Add a product to cart or change its quantity
+        :param product:
+        :param quantity:
+        :param update_quantity:
+        :return:
+        """
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
+        if update_quantity:
+            self.cart[product_id]['quantity'] = quantity
+        else:
+            self.cart[product_id]['quantity'] += quantity
+        self.save()
+
+    def save(self):
+        self.session[settings.CART_SESSION_ID] = self.cart
+        self.session.modified = True
+
+    def remove(self, product):
+        """
+        Remove product from shopping cart
+        :param product:
+        :return:
+        """
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
+
+    def clear(self):
+        del self.session[settings.CART_SESSION_ID]
+        self.session.modified = True
