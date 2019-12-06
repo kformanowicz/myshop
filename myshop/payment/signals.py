21ddb7bd0c2ab994e5f6c8ca73daf5da3c8ba0b2
Krzysztof Formanowicz <formanowicz.k@gmail.com>
2019-12-06 11:42:53 +0400

    sending email after successful payment

--- myshop/payment/signals.py
+++ myshop/payment/signals.py
@@ -1,6 +1,12 @@
+from django.conf import settings
+from django.core.mail import EmailMessage
 from django.shortcuts import get_object_or_404
+from django.template.loader import render_to_string
 from paypal.standard.models import ST_PP_COMPLETED
 from paypal.standard.ipn.signals import valid_ipn_received
+from io import BytesIO
+import weasyprint
+
 from orders.models import Order
 
 
@@ -10,6 +16,15 @@ def payment_notification(sender, **kwargs):
         order = get_object_or_404(Order, id=ipb_obj.invoice)
         order.paid = True
         order.save()
+        subject = "My shop - invoice no {}".format(order.id)
+        message = "Please find the invoice for your last order attached."
+        email = EmailMessage(subject, message, 'admin@myshop.com', [order.email])
+        html = render_to_string('admin/orders/order/pdf.html', {'order': order})
+        out = BytesIO()
+        stylesheets = [weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')]
+        weasyprint.HTML(string=html).write_pdf(out, stylesheets=stylesheets)
+        email.attach('order_{}.pdf'.format(order.id), out.getvalue(), 'application/pdf')
+        email.send()
 
 
 valid_ipn_received.connect(payment_notification)
